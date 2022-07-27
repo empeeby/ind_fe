814a80f556b010d150aa0bd59042fc2b37b1b370
Mike Parr-Burman <mike@empeeby.com>
2022-07-27 16:20:47 +0100

    number input sketch

--- templates.js
+++ templates.js
@@ -2,38 +2,6 @@ var templates ={
 
     notImplemented: 'template not implemented',
 
-    ptRetrieval2: '\
-    <h2 class=\'title\'></h2>\
-    <h3>input table</h3>\
-    <table class=\'input_table\'>\
-    </table>\
-    <button class=\'add-input-row-button\'>add row</button>\
-    <div class=\"params\">\
-        <h3>parameters</h3>\
-        \
-        <div class="wmodel-wrap">\
-            <h4>wmodel:</h4>\
-            <div class=\"wmodel\">\
-            </div>\
-        </div>\
-        \
-        <div class=\"dataset-wrap">\
-            <h4>dataset:</h4>\
-            <div class=\"dataset\">\
-            </div>\
-        </div>\
-        \
-        <div class=\"variant-wrap">\
-            <h4>variant:</h4>\
-            <div class=\"variant\">\
-            </div>\
-        </div>\
-    </div>\
-    <button class=\'submit-button\'>submit</button>\
-    <h3>output table</h3>\
-    <table class=\'output_table\'></table>\
-    ',
-
     ptRetrieval: `
     <h2 class="title"></h2>
     <h3>input table</h3>
@@ -56,8 +24,15 @@ var templates ={
             <div class="variant">
             </div>
         </div>
+        <div class="limit-wrap">
+            <h4>limit:</h4>
+            <div class="limit">
+                <label for="limit">Limit number of results per query (0 indicates no limit):</label>
+                <input type="number" id="limit" name="limit" min="0" value="0">
+            </div>
+        </div>
     </div>
-    <button class="submit-button">submit</button>
+    <!--<button class="submit-button">submit</button>-->
     <h3>output table</h3>
     <table class="output_table"></table>
     `,
