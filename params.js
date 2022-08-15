b425d5765d303943453135dc72a10692d57b22bf
Mike Parr-Burman <mike@empeeby.com>
2022-08-15 10:17:49 +0100

    added remote server url

--- params.js
+++ params.js
@@ -1,4 +1,5 @@
-var API_BASE_URL = 'http://127.0.0.1:8888/';
+// var API_BASE_URL = 'http://127.0.0.1:8888/';
+var API_BASE_URL = 'https://ind-api-demo.macavaney.us/';
 var DEBOUNCE_TIMEOUT = 200; // in ms
 var FORCE_INT_INPUT = ['docid']
 var SOFT_CONVERT_TO_NUMBER = ['rank', 'score']
